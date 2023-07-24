75901131b20ee140915262047c53d8d58dd050a7
korewadavid <korewadavid@gmail.com>
2023-07-24 15:26:39 +0200

    Updated ft_atoi

--- lvl2/ft_atoi/ft_atoi.c
+++ lvl2/ft_atoi/ft_atoi.c
@@ -1,1 +1,32 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int		ft_atoi(const char *str)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (str[i] == ' ' || str[i] == '	')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * result);
+}
+
+int main()
+{
+	const char *str = "    -042   97";
+
+	printf("%d\n", atoi(str));
+	printf("%d\n", ft_atoi(str));
+	return (0);
+}
