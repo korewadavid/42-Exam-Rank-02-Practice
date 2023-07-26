672be35177248ea34fba6a139c1b8c252fe024a1
korewadavid <korewadavid@gmail.com>
2023-07-26 14:47:10 +0200

    updated ft_strrev

--- lvl2/ft_strrev/ft_strrev.c
+++ lvl2/ft_strrev/ft_strrev.c
@@ -1,6 +1,27 @@
 #include <unistd.h>
+#include <stdio.h>
 
 char	*ft_strrev(char *str)
 {
+	int i = 0;
+	int len = 0;
+	char temp;
 
+	while (str[len] != '\0')
+		len++;
+	while (i <= len)
+	{
+		temp = str[i];
+		str[i] = str[len];
+		str[len] = len;
+		i++;
+		len--;
+	}
+	return (str);
+}
+
+int main()
+{
+	printf("%s\n", ft_strrev("abc"));
+	return(0);
 }
