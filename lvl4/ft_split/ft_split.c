a731aa6b691ea47ab710977d8854480fdb6ca78f
korewadavid <korewadavid@gmail.com>
2023-07-31 15:31:24 +0200

    added ft_split

---
+++ lvl4/ft_split/ft_split.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+//	Subcomponents of a project (slot machine 
+
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spn the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+
+char    **ft_split(char *str)
+{
+
+}
