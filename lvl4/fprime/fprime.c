0f113f9fb2df8ca617ba5d1e3ef2a0fd58e4548d
korewadavid <korewadavid@gmail.com>
2023-07-31 13:55:49 +0200

    updated fprime

---
+++ lvl4/fprime/fprime.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+//Prime factors: Numbers that only factor is 1 and itself
+
+// Get positive int from user (input is always valid)
+// Change num into an integer
+// Declare pFactor counter int to value of 2
+// Find first prime factor of the int and print
+// Equal pFactor to 2
+// Find first prime factor of the result of num / firstPrime
+// Print '*' followed by the new prime factor
+// Do this until you get to a number thats only prime factor is itself and print
+// Display a newline and return 0
+
+
