c91bfb1251056173870680714853bfb6238b2d70
korewadavid <korewadavid@gmail.com>
2023-07-31 15:17:37 +0200

    updated and completed fprime

--- lvl4/fprime/fprime.c
+++ lvl4/fprime/fprime.c
@@ -1,15 +1,47 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 //Prime factors: Numbers that only factor is 1 and itself
 
-// Get positive int from user (input is always valid)
-// Change num into an integer
-// Declare pFactor counter int to value of 2
-// Find first prime factor of the int and print
-// Equal pFactor to 2
-// Find first prime factor of the result of num / firstPrime
-// Print '*' followed by the new prime factor
-// Do this until you get to a number thats only prime factor is itself and print
-// Display a newline and return 0
+// Get positive int from user = num
+// In the case that num = 1 we can print 1
+// While the number is at least equal or greater then the first prime factor (if not it wont have any prime factors)
+	// If pFactor is a prime factor of num,
+		// Print the current prime factor
+		// Equal num to the number that we are now searching for its prime factor
+		// If num is equal or greater than the first prime factor write '*' (on the last prime factor num will equal 1 because num = fPrime)
+		// Subtract fPrime by 1
+	// If not, increment fPrime by 1 (this works for using the same prime factor in the if statment, and for going to the next possible prime factor)
 
+void	fprime(int num)
+{
+	if (num == 1)
+		printf("1");
+	else
+	{
+		int pFactor = 2;
+		while (num >= 2)
+		{
+			if (num % pFactor == 0)
+			{
+				printf("%d", pFactor);
+				num /= pFactor;
+				if (num >= 2)
+					printf("*");
+				pFactor--;
+			}
+			pFactor++;
+		}
+	}
+}
 
+int		main (int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		fprime(atoi(argv[1]));
+	}
+	printf("\n");
+	return (0);
+}
