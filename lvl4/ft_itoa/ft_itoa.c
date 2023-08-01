c1cdf976d7f9470cd504e491e849a15b94329a1f
korewadavid <korewadavid@gmail.com>
2023-08-01 18:40:55 +0200

    Update ft_itoa.c

---
+++ lvl4/ft_itoa/ft_itoa.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Step 1: Special case: Handle the minimum integer value (-2147483648)
+// Step 2: Determine the number of digits in the integer's string representation
+// Step 3: Allocate memory for the string (plus one additional byte for null terminator)
+// Step 4: Handle zero and negative numbers
+// Step 5: Convert the integer to the string representation
+// Step 6: Return the resulting string
